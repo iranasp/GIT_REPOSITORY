472ee7a9845b674f0a37345d988099270e85521e
IRAN-ASP <iranasp.com@gmail.com>
2018-07-23 20:44:59 +0430

    970501_20:44

---
+++ FormGenerator/FormGenerator/DAL/ConnectionString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ConnectionString
+    {
+        /// <summary>
+        /// Get database address
+        /// </summary>
+        public static string Database_ConnectionString
+        {
+            get { return ConfigurationManager.AppSettings["GeneratorConnection"]; }
+        }
+    }
+}
---
+++ FormGenerator/FormGenerator/DAL/dbTools.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class dbTools : P_DLL.dbObject
+    {
+        public static string Global_FormGenerator_Connection { get; set; }
+        public static string Global_FileForSaveMessage { get; set; }
+
+        public dbTools()
+        {
+            this.dbConnection = Global_FormGenerator_Connection;
+            this.FileForSaveMessage = Global_FormGenerator_Connection;
+        }
+
+        public dbTools(string iSqlStr, SqlSP iSqlSp)
+        {
+            this.dbConnection = Global_FormGenerator_Connection;
+            this.FileForSaveMessage = Global_FileForSaveMessage;
+
+            this.CommandText = iSqlStr;
+            this.CommandType = iSqlSp;
+        }
+
+        public dbTools(string iSqlStr)
+        {
+            this.dbConnection = Global_FormGenerator_Connection;
+            this.FileForSaveMessage = Global_FileForSaveMessage;
+
+            this.CommandText = iSqlStr;
+
+            if (iSqlStr.IndexOf(" ") == -1)
+            {
+                this.CommandType = P_DLL.dbObject.SqlSP.SP;
+            }
+            else
+            {
+                this.CommandType = P_DLL.dbObject.SqlSP.SQL;
+            }
+        }
+
+    }
+}
